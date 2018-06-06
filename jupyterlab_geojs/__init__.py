c2e3a0da3055eb5deb3552816f2ef7983c96f66a
John Tourtellott <john.tourtellott@kitware.com>
2018-06-06 16:37:28 -0400

    Step 1: remove all files not used for typescript build

--- jupyterlab_geojs/__init__.py
+++ jupyterlab_geojs/__init__.py
@@ -1,12 +1,13 @@
+version_info = (0, 2, 0, 'dev')
+__version__ = ".".join(map(str, version_info))
+
 from .geojsmap import GeoJSMap
 
-# Running `npm run build` will create static resources in the static
-# directory of this Python package (and create that directory if necessary).
 
-def _jupyter_nbextension_paths():
-    return [{
-        'section': 'notebook',
-        'src': 'nbextension',
-        'dest': 'jupyterlab_geojs',
-        'require': 'jupyterlab_geojs/extension'
-    }]
+# def _jupyter_nbextension_paths():
+#     return [{
+#         'section': 'notebook',
+#         'src': 'nbextension',
+#         'dest': 'jupyterlab_geojs',
+#         'require': 'jupyterlab_geojs/extension'
+#     }]
